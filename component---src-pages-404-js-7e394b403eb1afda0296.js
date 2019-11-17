d89728266e71ac349709384d17399f6543a684c8
Burak Sümer <buraksumer13@gmail.com>
2019-11-17 22:25:21 +0300

    Favicon changed

---
+++ component---src-pages-404-js-7e394b403eb1afda0296.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{328:function(n,e,c){"use strict";c.r(e);var l=c(5),t=(c(2),c(322));e.default=function(){return Object(l.c)(t.a,null,Object(l.c)("h1",null,"BULUNAMADI"),Object(l.c)("p",null,"Öyle bir yere geldin ki, burada üzüntü bile yok."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-7e394b403eb1afda0296.js.map
